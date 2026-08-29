0b9522606f147cfd61e543918f07010ac8bc401d
Sarika Kushwaha <sarika.vsa01@gmail.com>
2026-08-29 15:48:42 +0530

    Update insecure_app.py

--- insecure_app.py
+++ insecure_app.py
@@ -1,20 +1,14 @@
+from flask import Flask, request
 import os
-import sqlite3
-
-def run_command(user_input):
+ 
+app = Flask(__name__)
+ 
+@app.route("/ping")
+def ping():
     # Command Injection Vulnerability
-    os.system("ping -c 1 " + user_input)
-
-def get_user(user_id):
-    # SQL Injection Vulnerability
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM users WHERE id = '%s'" % user_id)
-    return cursor.fetchall()
-
-# Tainted Inputs
-command_input = "127.0.0.1; cat /etc/passwd"
-user_id_input = "1' OR '1'='1"
-
-run_command(command_input)
-get_user(user_id_input)
+    ip = request.args.get("ip")
+    os.system("ping -c 1 " + ip)
+    return "Pinging " + ip
+ 
+if __name__ == "__main__":
+    app.run()
